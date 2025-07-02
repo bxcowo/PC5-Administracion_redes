ae0ddbf855af404088676668ddeb81100a965011
bxcowo <a.flores.a@uni.pe>
2025-07-02 00:40:49 -0500

    feat: Create simple bash script to automate running the application.

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker-compose up --build -d
+
+# Setup Python environment
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+
+# Run the application
+python3 main.py
