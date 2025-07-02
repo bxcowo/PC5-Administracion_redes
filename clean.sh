af5a0f53ec2a02e4ab6b36b97716ddbb3d2e4a7c
bxcowo <a.flores.a@uni.pe>
2025-07-02 01:35:23 -0500

    feat: update clean.sh to delete orphans and delete .pytest_cache directories

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker-compose down -v
+docker-compose down -v --remove-orphans
 rm -rf .venv
 find . -type d -name "__pycache__" -exec rm -rf {} +
+find . -type d -name ".pytest_cache" -exec rm -rf {} +
