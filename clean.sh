deb35cdb12484fd618cce62fbc1d306a38c7ab53
bxcowo <a.flores.a@uni.pe>
2025-07-03 16:08:28 -0500

    feat: update clean.sh to delete .coverage files created

--- clean.sh
+++ clean.sh
@@ -4,3 +4,4 @@ docker-compose down -v --remove-orphans
 rm -rf .venv
 find . -type d -name "__pycache__" -exec rm -rf {} +
 find . -type d -name ".pytest_cache" -exec rm -rf {} +
+find . -type f -name ".coverage" -exec rm -rf {} +
